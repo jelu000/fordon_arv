09877040db10df5e36e9405d8e8696c7c7a87afb
Jens <jelu000@gmail.com>
2023-03-04 17:58:53 +0100

    Nu verkar allt funka

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ looping = True
 
 #Initierar 2 st personbils object av classen Personbil
 opel_brun = personbil.Personbil("Opel", "Brun", 200)
-volvo_svart = personbil.Personbil("Opel", "Svart", 250)
+volvo_svart = personbil.Personbil("Volvo", "Svart", 250)
 
 #Initierar 2 st lastbils object av classen Lastbil
 scania_rosa = lastbil.Lastbil("Scania", "Rosa", 40000)
@@ -15,23 +15,47 @@ volvobm_orange = lastbil.Lastbil("Volvo BM", "Orange", 50000)
 personbils_lista = [opel_brun, volvo_svart]
 lastbils_lista = [scania_rosa, volvobm_orange]
 
+#----------------------------------------------
+#function som listar valt forton
+def print_fordonslista(fordonslista):
+    
+    for ettfordon in fordonslista:
+        #print(ettfordon.fabrikat +" Typ="+ str(type(ettfordon)))
+        
+        #kollar om ett fordon är en intance av Klassen lastbil eller personbil
+        if(isinstance(ettfordon  , lastbil.Lastbil)):
+            print(ettfordon.fabrikat +" Färg: "+ ettfordon.color + ", Flakvolym: " + str(ettfordon.flakvolym) +  " L")
+
+        elif(isinstance(ettfordon  ,personbil.Personbil)):
+            print(ettfordon.fabrikat +" Färg: "+ ettfordon.color + ", Bagagevolym: " + str(ettfordon.bagagevolym) +  " L")
+
+#---------------------------------------------
+
+#main program
 while looping:
     print("----------------------------------------")
     print("\n-:Klasser och arv av fordon:-\n")
 
-    val_fordon = input("Vilken fordonstyp vill du lista? 1=lastbilar : 2=personbilar")
+    val_fordon = input("Vilken fordonstyp vill du lista? 1=lastbilar : 2=personbilar: ")
+    
 
     if (val_fordon == "1"):
-        print("lastbil")
+        print("\n-:LastBilar:-")
+        print("-----------------------------------")
+        print_fordonslista(lastbils_lista)
     
     elif (val_fordon == "2"):
-        print("personbil")
+        print("\n-:PersonBilar:-")
+        print("-----------------------------------")
+        print_fordonslista(personbils_lista)
 
     else:
-        print("ogiltigt val")
+        print("n\nogiltigt val")
 
+    print("------------------------------------------")
     go = input("\n Vill du lista fordon igen? j/n ")
     print("\n")
     
     if (go == "n"):
         break
+
